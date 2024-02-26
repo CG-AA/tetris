b25ec61ce4b702fd7098fd856de5f90d4bab0dd8
CG-TW <117447927+CG-AA@users.noreply.github.com>
2024-02-26 14:42:43 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+const dot = [0, 0]
+class struct_alive {
+    constructor(struct_add) {
+        new struct_alive(struct_add)
+    }
+}
+function hartbeat() {
+
+}
+basic.forever(function () {
+    basic.pause(1000)
+
+})
