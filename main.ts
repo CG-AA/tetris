b9ab7102a9062e953f4c416dd0a57137cd848f20
CG-TW <117447927+CG-AA@users.noreply.github.com>
2024-02-26 15:05:34 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,31 @@
-const dot = [0, 0]
-class struct_alive {
-    constructor(struct_add) {
-        new struct_alive(struct_add)
-    }
-}
-function hartbeat() {
+let x = 2;
+let y = 0;
+let move_place = 0;
 
+input.onButtonPressed(Button.A, function () {
+    move_place = -1;
+});
+
+input.onButtonPressed(Button.B, function () {
+    move_place = 1;
+});
+
+function move(){
+    if(move_place == 0){
+        return;
+    } else if (move_place + x > 0 && move_place + x < 4){
+        
+    } 
+    move_place = 0;
 }
+
 basic.forever(function () {
-    basic.pause(1000)
-
-})
+    led.unplot(x, y);
+    y++;
+    if (y > 4) {
+        y = 0;
+        x = Math.floor(Math.random() * 5);
+    }
+    led.plot(x, y);
+    basic.pause(1000);
+});
